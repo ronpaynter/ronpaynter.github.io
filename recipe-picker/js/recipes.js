ff0b1f7cfc1281863f06b1f37181d824cb40c9b8
Ron Paynter <ronpaynter@yahoo.com>
2026-02-07 10:38:28 -0500

    Add Recipe Picker

---
+++ recipe-picker/js/recipes.js
@@ -0,0 +1,70 @@
+
+	var recipes = [
+	{
+	  "name": "Cinnamon Roasted Almonds",
+	  "type": "Appetizers & Snacks"
+	}, {
+	  "name": "Pretzels",
+	  "type": "Appetizers & Snacks"
+	}, {
+	  "name": "Griddle Cakes",
+	  "type": "Breakfast"
+	}, {
+	  "name": "Uncle Bob's French Toast",
+	  "type": "Breakfast"
+	}, {
+	  "name": "Waffles",
+	  "type": "Breakfast"
+	}, {
+	  "name": "Molasses Cookies",
+	  "type": "Cookies"
+	}, {
+	  "name": "Peanut Blossoms",
+	  "type": "Cookies"
+	}, {
+	  "name": "Snickerdoodles",
+	  "type": "Cookies"
+	}, {
+	  "name": "Sugar Cookies",
+	  "type": "Cookies"
+	}, {
+	  "name": "Mud Pie",
+	  "type": "Desserts"
+	}, {
+	  "name": "Sour Cream Rolls",
+	  "type": "Desserts"
+	}, {
+	  "name": "Alfredo Sauce",
+	  "type": "Entrees"
+	}, {
+	  "name": "Bread Machine Calzone",
+	  "type": "Entrees"
+	}, {
+	  "name": "Chicken Enchiladas",
+	  "type": "Entrees"
+	}, {
+	  "name": "Crab and Shrimp Stuffed Shells",
+	  "type": "Entrees"
+	}, {
+	  "name": "Easy Manicotti",
+	  "type": "Entrees"
+	}, {
+	  "name": "Pizza Dough",
+	  "type": "Entrees"
+	}, {
+	  "name": "Mint Chocolate Chip Ice Cream",
+	  "type": "Ice Cream"
+	}, {
+	  "name": "Broccoli Casserole",
+	  "type": "Side Dishes"
+	}, {
+	  "name": "Scalloped Potatoes",
+	  "type": "Side Dishes"
+	}, {
+	  "name": "Crock Pot Chicken & Dumplings",
+	  "type": "Soups, Stews, and Chili"
+	}, {
+	  "name": "Potato Soup",
+	  "type": "Soups, Stews, and Chili"
+	}
+	  ];
