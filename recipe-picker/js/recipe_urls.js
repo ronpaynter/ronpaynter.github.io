7a2d2328c0bf11fb4ae8fca09125f5a13d8ec083
ronpaynter <rpaynter@textilemanagement.com>
2026-02-10 13:10:23 -0500

    WIP.

---
+++ recipe-picker/js/recipe_urls.js
@@ -0,0 +1,82 @@
+
+	var recipe_urls = [
+	{
+	  "name": "Cinnamon Roasted Almonds",
+	  "url": "Appetizers&Snacks/CinnamonRoastedAlmonds"
+	}, {
+	  "name": "Pretzels",
+	  "url": "Appetizers&Snacks/Pretzels"
+	}, {
+	  "name": "Griddle Cakes",
+	  "url": "Breakfast/GriddleCakes"
+	}, {
+	  "name": "Uncle Bob's French Toast",
+	  "url": "Breakfast/UncleBobsFrenchToast"
+	}, {
+	  "name": "Waffle House Waffles",
+	  "url": "Breakfast/WaffleHouseWaffles"
+	}, {
+	  "name": "Waffles",
+	  "url": "Breakfast/Waffles"
+	}, {
+	  "name": "Molasses Cookies",
+	  "url": "Cookies/MolassesCookies"
+	}, {
+	  "name": "Peanut Blossoms",
+	  "url": "Cookies/PeanutBlossoms"
+	}, {
+	  "name": "Snickerdoodles",
+	  "url": "Cookies/Snickerdoodles"
+	}, {
+	  "name": "Sugar Cookies",
+	  "url": "Cookies/SugarCookies"
+	}, {
+	  "name": "Blackberry Cobbler",
+	  "url": "Desserts/BlackberryCobbler"
+	}, {
+	  "name": "Mom's Apple Dumplings",
+	  "url": "Desserts/MomsAppleDumplings"
+	}, {
+	  "name": "Mud Pie",
+	  "url": "Desserts/MudPie"
+	}, {
+	  "name": "Pecan Pie",
+	  "url": "Desserts/PecanPie"
+	}, {
+	  "name": "Sour Cream Rolls",
+	  "url": "Desserts/SourCreamRolls"
+	}, {
+	  "name": "Alfredo Sauce",
+	  "url": "Entrees/AlfredoSauce"
+	}, {
+	  "name": "Bread Machine Calzone",
+	  "url": "Entrees/BreadMachineCalzone"
+	}, {
+	  "name": "Chicken Enchiladas",
+	  "url": "Entrees/ChickenEnchiladas"
+	}, {
+	  "name": "Crab and Shrimp Stuffed Shells",
+	  "url": "Entrees/CrabandShrimpStuffedShells"
+	}, {
+	  "name": "Easy Manicotti",
+	  "url": "Entrees/EasyManicotti"
+	}, {
+	  "name": "Pizza Dough",
+	  "url": "Entrees/PizzaDough"
+	}, {
+	  "name": "Mint Chocolate Chip Ice Cream",
+	  "url": "IceCream/MintChocolateChipIceCream"
+	}, {
+	  "name": "Broccoli Casserole",
+	  "url": "SideDishes/BroccoliCasserole"
+	}, {
+	  "name": "Scalloped Potatoes",
+	  "url": "SideDishes/ScallopedPotatoes"
+	}, {
+	  "name": "Crock Pot Chicken & Dumplings",
+	  "url": "Soups,Stews,andChili/CrockPotChicken&Dumplings"
+	}, {
+	  "name": "Potato Soup",
+	  "url": "Soups,Stews,andChili/PotatoSoup"
+	}
+	  ];
